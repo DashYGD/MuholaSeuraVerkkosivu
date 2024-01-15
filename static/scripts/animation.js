1fa7b3a7f88293208a052f3200b32b85429ee8fe
EliahSan <114251134+DashYGD@users.noreply.github.com>
2024-01-15 13:11:17 +0200

    Pienillä näytöillä animaatio on fade-in Isoilla näytöillä animaatio on fade-in-top

--- static/scripts/animation.js
+++ static/scripts/animation.js
@@ -1,7 +1,15 @@
-
 window.addEventListener('load', function() {
-    setTimeout(function() {
-        document.getElementById('base').classList.add('w3-animate-top');
-        document.getElementById('base').style.opacity = "100%";
-    }, 500); // Adjust the delay as needed
+    function addAnimation() {
+        setTimeout(function () {
+            document.getElementById('base').classList.add('w3-animate-top');
+            document.getElementById('base').style.opacity = "100";
+        }, 500); // Adjust the delay as needed
+    }
+    // Check screen width on load and add animation if below 1024px
+    if (window.innerWidth > 991) {
+        addAnimation();
+    } else {
+        document.getElementById('base').style.transition = "opacity 0.5s ease-in-out";
+        document.getElementById('base').style.opacity = "1";
+    }
 });
