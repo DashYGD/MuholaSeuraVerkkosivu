871bf8a04c3cf9f8bbb4096da64dec7fbff4b209
VeetiRauta <150324714+VeetiRauta@users.noreply.github.com>
2024-01-24 09:59:27 +0200

    login.php ulkoasun päivitys

--- kirjaudu/login.php
+++ kirjaudu/login.php
@@ -13,6 +13,13 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="../static/styles/core.css">
     </head>
+    
+    <style>
+      *:focus {
+        outline: none;
+      }
+    </style>
+
 <body>
 
 <div class="w3-container">
@@ -55,37 +62,25 @@
         </div>
       </div>
 
+
     <center>
 <h2>Muhola Seura Ry Kirjautuminen</h2>
-<button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-blue w3-large" style="margin-bottom: 20px;">Kirjaudu</button>
 </center>
-
-
-<div id="id01" class="w3-modal">
-    <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width: 600px">
-
-        <div class="w3-center"><br>
-            <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
-            <img src="images/avatar2.jpg" alt="Avatar" style="width:30%" class="w3-circle w3-margin-top">
-        </div>
 
         <form class="w3-container" action="action_page" method="POST">
             <div class="w3-section">
-                <label><b>Käyttäjänimi</b></label>
-                <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Aseta käyttäjänimi" name="usrname" required>
+                <label><b>Käyttäjänimi/Sähköposti</b></label>
+                <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Aseta käyttäjänimi tai sähköposti" name="usrname" required>
                 <label><b>Salasana</b></label>
                 <input class="w3-input w3-border" type="password" placeholder="Aseta salasana" name="psw" required>
-                <button class="w3-button w3-block w3-green w3-section w3-padding" type="submit">Kirjaudu</button>
                 <input class="w3-check w3-margin-top" type="checkbox" checked="checked"> Muista minut</button>
+                <span class="w3-right w3-margin-top w3-padding w3-hide-small">Unohditko <a href="#">salasanasi?</a></span>
+                <button class="w3-button w3-block w3-green w3-section w3-padding" type="submit">Kirjaudu</button>
+                
             </div>
         </form>
-        
-        <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
-            <button onclick="document.getElementById('id01').style.display='none'" type="button" class="w3-button w3-red">Peruuta</button>
-            <span class="w3-right w3-padding w3-hide-small">Unohditko <a href="#">salasanasi?</a></span>
-        </div>
-    </div>
-</div>
+    
+
 <script type="text/javascript" src="../static/scripts/animation.js"></script>
   <script type="text/javascript" src="../static/scripts/keyboard-accessibility.js"></script>
   <script type="text/javascript" src="../static/scripts/scrollposition.js"></script>
